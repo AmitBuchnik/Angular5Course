a38290705e1a6752365dbcd15b29bd0e4ec09cc3
amitbuhnik@gmail.com <amitbuhnik@gmail.com>
2018-03-20 20:30:25 +0200

    pipe

--- src/app/reverse-string.pipe.ts
+++ src/app/reverse-string.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ReverseStringPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
+  transform(value: string, args?: any): any {
     return value.split("").reverse().join("");
   }
 }
